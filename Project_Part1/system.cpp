4ba107f3a8c64dc4dfd442458875958478c5f23f
espar <espar@DESKTOP-EE>
2025-02-12 23:34:18 -0600

    added startup delay

--- Project_Part1/system.cpp
+++ Project_Part1/system.cpp
@@ -7,15 +7,37 @@ using namespace std;
 
 void bootUp() {
     cout << "Booting up the OS...\n";
+    this_thread::sleep_for(chrono::milliseconds(1580));
 
-    if (!filesystem::exists("system_config.cfg")) {
-        cout << "System configuration file not found. Creating default config...\n";
-        // You can add code here to create a config file if needed.
-    }
-    else {
-        cout << "System configuration found.\n";
-    }
+    cout << "[BOOT] Initializing system... \n";
+    this_thread::sleep_for(chrono::milliseconds(120));
+    cout << "[BOOT] Performing startup checks... \n";
+    this_thread::sleep_for(chrono::milliseconds(250));
+    cout << "[BOOT] Checking memory...";
+    this_thread::sleep_for(chrono::milliseconds(80));
+    cout << " OK \n[BOOT] Checking storage...";
+    this_thread::sleep_for(chrono::milliseconds(300));
+    cout << " OK \n[BOOT] Verifying boot sector...";
+    this_thread::sleep_for(chrono::milliseconds(60));
 
-    this_thread::sleep_for(chrono::seconds(2));
+    cout << " OK \n[LOADER] Loading core system files... \n";
+    this_thread::sleep_for(chrono::milliseconds(300));
+    cout << "[LOADER] Mounting file system... \n";
+    this_thread::sleep_for(chrono::milliseconds(140));
+    cout << "[LOADER] Configuring system services... \n";
+    this_thread::sleep_for(chrono::milliseconds(420));
+
+    cout << "[KERNEL] Starting system processes... \n";
+    this_thread::sleep_for(chrono::milliseconds(680));
+    cout << "[KERNEL] Initializing drivers... \n";
+    this_thread::sleep_for(chrono::milliseconds(900));
+    cout << "[KERNEL] Establishing network connection...\n";
+    this_thread::sleep_for(chrono::milliseconds(120));
+
+    cout << "[SYSTEM] Launching user interface... \n";
+    this_thread::sleep_for(chrono::milliseconds(200));
+    cout << "[SYSTEM] Finalizing startup... \n";
+    this_thread::sleep_for(chrono::milliseconds(600));
+
     cout << "Boot process complete.\n";
 }
